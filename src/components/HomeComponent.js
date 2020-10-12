58840700f33b821fde4b8f3ce865aca65e8ec9d1
luke thao <chiaseathao@gmail.com>
2020-10-12 15:40:08 -0500

    Single Page Applications pt. 1

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import {Card, CardImg, CardText, CardTitle, CardBody, CardSubtitle} from 'reactstrap'
+
+
+
+function RenderCard({item}) {
+    return (
+        <Card>
+            <CardImg src={item.image} alt={item.name}/>
+            <CardBody>
+                <CardTitle style={{fontWeight: 'bold'}}>{item.name}</CardTitle>
+                {item.designation ? <CardSubtitle style={{fontWeight: 'bold'}}>{item.designation}</CardSubtitle> : null}
+                <CardText>
+                    {item.description}
+                </CardText>
+            </CardBody>
+        </Card>
+    );
+}
 
 function Home(props) {
-    return(
+    return (
         <div className="container">
-            <h4>Home</h4>
+            <div className="row align-items-start">
+                <div className="col-12 col-md m-1">
+                    <RenderCard item={props.dish}/>
+                </div>
+                <div className="col-12 col-md m-1">
+                    <RenderCard item={props.promotions}/>
+                </div>
+                <div className="col-12 col-md m-1">
+                    <RenderCard item={props.leaders}/>
+                </div>
+            </div>
         </div>
     );
 }

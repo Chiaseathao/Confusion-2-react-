d1bce55cf824d5df4c1c8393bd17b0f99c883efc
luke thao <chiaseathao@gmail.com>
2020-12-09 16:48:07 -0600

    fetch from server

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -14,6 +14,7 @@ import {
 import {Link} from "react-router-dom";
 import {Control, Errors, LocalForm} from "react-redux-form";
 import { Loading } from "./LoadingComponent";
+import { baseUrl } from "../shared/baseUrl";
 
 
 const maxLength = (len) => (val) => !(val) || (val.length <= len)
@@ -118,7 +119,7 @@ function RenderDish ({dish}) {
 
     return (
         <Card>
-            <CardImg width="100%" src={dish.image} alt={dish.name}/>
+            <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name}/>
             <CardBody>
                 <CardTitle style={{fontWeight: "bold"}}>{dish.name}</CardTitle>
                 <CardText>{dish.description}</CardText>
